8ce8e8e3e4063db32db314a81f1b4ec7a0f95e9b
jdaltamirano <1668@holbertonschool.com>
2020-02-13 22:57:36 +0000

    print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_square - If else statement
+ * @x:
+ * @size: integer number
+ * Return: 0 (Succes)
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	y = 0;
+
+	if (size < 1)
+		_putchar('\n');
+
+	while (y < size)
+	{
+		x = 0;
+		while (x < size)
+		{
+			_putchar('#');
+			x++;
+		}
+		_putchar('\n');
+		y++;
+	}
+}
