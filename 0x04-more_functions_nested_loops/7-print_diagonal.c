b58ab513944475b3ebfc9dd03f6f2ff83f73504a
jdaltamirano <1668@holbertonschool.com>
2020-02-13 22:49:28 +0000

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - If else statement
+ * @n: integer number
+ * Return: 0 (Succes)
+ */
+
+void print_diagonal(int n)
+{
+	int c, i;
+
+	c = 0;
+
+	while (n > 0)
+	{
+		i = c;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		c++;
+		n--;
+	}
+	if (c < 1)
+		_putchar('\n');
+}
