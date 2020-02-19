e5a5d23e779ae3b57713a3c5268a81b150403611
jdaltamirano <1668@holbertonschool.com>
2020-02-19 01:45:04 +0000

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,22 @@
 #include "holberton.h"
 
 /**
- * rev_string
- * @
+ * rev_string - If else statement
+ * @s: char
  * Return: 0 (Sucess)
  */
 void rev_string(char *s)
 {
-	int n, i, j = 0;
-	char temporal[100];
+	int n, i;
+	char temporal;
 
-	for (n = 0; s[n] != '\0'; n++)
+	for (n = 0; *(s + n) != '\0'; n++)
+	;
+	n--;
+	for (i = 0; i < n; i++, n--)
 	{
-		temporal[n] = s[n];
+		temporal = *(s + n);
+		*(s + n) = *(s + i);
+		*(s + i) = temporal;
 	}
-	for (i = (n - 1); i >= 0; i--)
-	{
-		s[j] = temporal[i];
-		j++;
-		
-	}
-	
 }
