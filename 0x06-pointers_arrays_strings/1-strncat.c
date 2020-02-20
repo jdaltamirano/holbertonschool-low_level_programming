e28c75b584d0e829655ddba7b81a868dee4e4e43
jdaltamirano <1668@holbertonschool.com>
2020-02-20 21:09:41 +0000

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * *_strncat - If else statement
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: 0 (Sucess)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	;
+
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
