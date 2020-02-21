3982a69ce39c43580496bbea75f24804c22a1259
jdaltamirano <1668@holbertonschool.com>
2020-02-21 02:49:18 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,12 @@
 int _strcmp(char *s1, char *s2)
 {
 
+	int i;
 
-	if (s1 < s2)
-	return (-15);
-	else if (s2 < s1)
-	return (15);
-	else
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
 	return (0);
 }
