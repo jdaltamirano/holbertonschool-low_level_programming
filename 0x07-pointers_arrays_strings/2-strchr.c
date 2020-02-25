67f5d0fb23c209180d516ef70bb73d4d3610ee0d
jdaltamirano <1668@holbertonschool.com>
2020-02-25 04:19:33 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+  
+/**
+* _strchr - If else statement
+* @s: char
+* @c: char
+* Return: 0 (Sucess)
+**/
+
+char *_strchr(char *s, char c)
+{
+	int i = 0, a = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i++;
+	while (a <= i)
+	{
+		if (s[a] == c)
+		{
+			return (s + a);
+		}
+		a++;
+	}
+	return ('\0');
+}
