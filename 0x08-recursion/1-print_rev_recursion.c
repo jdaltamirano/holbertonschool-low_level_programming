6177c177fe1622602fabaea756a5196010d8d9a4
jdaltamirano <1668@holbertonschool.com>
2020-02-26 22:13:04 +0000

    print rev recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,14 +8,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	int n;
-	int i;
-
-	for (n = 0; s[n] != '\0'; n++)
-	;
-	for (i = (n - 1); i >= 0; i--)
+	if (*s)
 	{
-		_putchar(s[i]);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_putchar('\n');
 }
