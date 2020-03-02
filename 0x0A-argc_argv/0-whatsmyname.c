1719c5602cabe0a916827cd77b878dc77371a557
jdaltamirano <1668@holbertonschool.com>
2020-03-02 02:53:39 +0000

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - If else statement
+ * @argc: int
+ * @argv: char
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
